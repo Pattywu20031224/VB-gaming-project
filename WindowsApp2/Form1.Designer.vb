58bf91bbb4aca3ddfc1e258cf8f6ad2fb045b78d
PeiWun <64179604+PeiWun@users.noreply.github.com>
2020-06-16 23:50:38 +0800

    Add files via upload

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.c1 = New System.Windows.Forms.Label()
+        Me.c2 = New System.Windows.Forms.Label()
+        Me.c3 = New System.Windows.Forms.Label()
+        Me.c6 = New System.Windows.Forms.Label()
+        Me.c5 = New System.Windows.Forms.Label()
+        Me.c4 = New System.Windows.Forms.Label()
+        Me.c9 = New System.Windows.Forms.Label()
+        Me.c8 = New System.Windows.Forms.Label()
+        Me.c7 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'c1
+        '
+        Me.c1.BackColor = System.Drawing.Color.White
+        Me.c1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c1.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c1.Location = New System.Drawing.Point(71, 33)
+        Me.c1.Name = "c1"
+        Me.c1.Size = New System.Drawing.Size(67, 72)
+        Me.c1.TabIndex = 0
+        '
+        'c2
+        '
+        Me.c2.BackColor = System.Drawing.Color.White
+        Me.c2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c2.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c2.Location = New System.Drawing.Point(182, 33)
+        Me.c2.Name = "c2"
+        Me.c2.Size = New System.Drawing.Size(67, 72)
+        Me.c2.TabIndex = 1
+        '
+        'c3
+        '
+        Me.c3.BackColor = System.Drawing.Color.White
+        Me.c3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c3.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c3.Location = New System.Drawing.Point(301, 33)
+        Me.c3.Name = "c3"
+        Me.c3.Size = New System.Drawing.Size(67, 72)
+        Me.c3.TabIndex = 2
+        '
+        'c6
+        '
+        Me.c6.BackColor = System.Drawing.Color.White
+        Me.c6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c6.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c6.Location = New System.Drawing.Point(301, 148)
+        Me.c6.Name = "c6"
+        Me.c6.Size = New System.Drawing.Size(67, 72)
+        Me.c6.TabIndex = 5
+        '
+        'c5
+        '
+        Me.c5.BackColor = System.Drawing.Color.White
+        Me.c5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c5.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c5.Location = New System.Drawing.Point(182, 148)
+        Me.c5.Name = "c5"
+        Me.c5.Size = New System.Drawing.Size(67, 72)
+        Me.c5.TabIndex = 4
+        '
+        'c4
+        '
+        Me.c4.BackColor = System.Drawing.Color.White
+        Me.c4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c4.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c4.Location = New System.Drawing.Point(71, 148)
+        Me.c4.Name = "c4"
+        Me.c4.Size = New System.Drawing.Size(67, 72)
+        Me.c4.TabIndex = 3
+        '
+        'c9
+        '
+        Me.c9.BackColor = System.Drawing.Color.White
+        Me.c9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c9.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c9.Location = New System.Drawing.Point(301, 262)
+        Me.c9.Name = "c9"
+        Me.c9.Size = New System.Drawing.Size(67, 72)
+        Me.c9.TabIndex = 8
+        '
+        'c8
+        '
+        Me.c8.BackColor = System.Drawing.Color.White
+        Me.c8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c8.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c8.Location = New System.Drawing.Point(182, 262)
+        Me.c8.Name = "c8"
+        Me.c8.Size = New System.Drawing.Size(67, 72)
+        Me.c8.TabIndex = 7
+        '
+        'c7
+        '
+        Me.c7.BackColor = System.Drawing.Color.White
+        Me.c7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.c7.Font = New System.Drawing.Font("新細明體", 48.0!)
+        Me.c7.Location = New System.Drawing.Point(71, 262)
+        Me.c7.Name = "c7"
+        Me.c7.Size = New System.Drawing.Size(67, 72)
+        Me.c7.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("標楷體", 39.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(460, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(264, 74)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "勝負判斷"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.Font = New System.Drawing.Font("標楷體", 39.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(71, 369)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(297, 69)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "再玩一次"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApp2.My.Resources.Resources.unnamed
+        Me.PictureBox1.Location = New System.Drawing.Point(455, 110)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(269, 242)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Black
+        Me.Button2.Font = New System.Drawing.Font("標楷體", 39.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(437, 369)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(297, 69)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "離開遊戲"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.c9)
+        Me.Controls.Add(Me.c8)
+        Me.Controls.Add(Me.c7)
+        Me.Controls.Add(Me.c6)
+        Me.Controls.Add(Me.c5)
+        Me.Controls.Add(Me.c4)
+        Me.Controls.Add(Me.c3)
+        Me.Controls.Add(Me.c2)
+        Me.Controls.Add(Me.c1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents c1 As Label
+    Friend WithEvents c2 As Label
+    Friend WithEvents c3 As Label
+    Friend WithEvents c6 As Label
+    Friend WithEvents c5 As Label
+    Friend WithEvents c4 As Label
+    Friend WithEvents c9 As Label
+    Friend WithEvents c8 As Label
+    Friend WithEvents c7 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Dim T As Boolean 'change player
+    Dim a As Integer
+    Private Sub c1_click(sender As Object, e As EventArgs) Handles c1.Click, c2.Click, c3.Click, c4.Click, c5.Click, c6.Click, c7.Click, c8.Click, c9.Click
+        If Label1.Text = "" Then '未分出勝負
+            If sender.Text = "" Then '棋格是空的
+                If T = True Then
+                    sender.Text = "X" '畫X
+                    a = a + 1
+                    Button1.Enabled = False
+                    Button2.Enabled = False
+                Else
+                    sender.Text = "O" '畫O
+                    a = a + 1
+                    Button1.Enabled = False
+                    Button2.Enabled = False
+                End If
+                T = Not (T) 'change player
+                If chkWin() <> "" Then
+                    Label1.Text = chkWin() + "贏了"
+                    PictureBox1.Visible = True
+                    My.Computer.Audio.Play(My.Application.Info.DirectoryPath & "\歡呼.wav",
+                    AudioPlayMode.Background)
+                    Button1.Enabled = True
+                    Button2.Enabled = True
+                ElseIf a = 9 Then
+                    Label1.Text = "平手"
+                        a = 0
+                        Button1.Enabled = True
+                    Button2.Enabled = True
+                End If
+
+            End If
+        End If
+    End Sub
+    '離開遊戲
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+        My.Computer.Audio.Stop()
+    End Sub
+    '清空棋盤
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load, Button1.Click
+        For Each c In Me.Controls  '表單上的每一個controls
+            If TypeOf (c) Is Label Then '如果是標籤物件
+                c.text = "" '清除文字內容
+                PictureBox1.Visible = False
+                a = 0
+                My.Computer.Audio.Stop()
+            End If
+        Next
+    End Sub
+    '判斷勝負
+    Function chkWin() As String
+        If c1.Text + c2.Text + c3.Text = "OOO" Then Return "O"
+        If c4.Text + c5.Text + c6.Text = "OOO" Then Return "O"
+        If c7.Text + c8.Text + c9.Text = "OOO" Then Return "O"
+        If c1.Text + c4.Text + c7.Text = "OOO" Then Return "O"
+        If c2.Text + c5.Text + c8.Text = "OOO" Then Return "O"
+        If c3.Text + c6.Text + c9.Text = "OOO" Then Return "O"
+        If c1.Text + c5.Text + c9.Text = "OOO" Then Return "O"
+        If c3.Text + c5.Text + c7.Text = "OOO" Then Return "O"
+        If c1.Text + c2.Text + c3.Text = "XXX" Then Return "X"
+        If c4.Text + c5.Text + c6.Text = "XXX" Then Return "X"
+        If c7.Text + c8.Text + c9.Text = "XXX" Then Return "X"
+        If c1.Text + c4.Text + c7.Text = "XXX" Then Return "X"
+        If c2.Text + c5.Text + c8.Text = "XXX" Then Return "X"
+        If c3.Text + c6.Text + c9.Text = "XXX" Then Return "X"
+        If c1.Text + c5.Text + c9.Text = "XXX" Then Return "X"
+        If c3.Text + c5.Text + c7.Text = "XXX" Then Return "X"
+        Return "" '圈叉都未連線
+    End Function
+End Class
